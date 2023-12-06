224a22f7267ce7ec792527ebf7a9d10f66dfbc9e
Michael <michaelzd95@gmail.com>
2023-12-05 21:19:11 -0500

    part1

--- src/day1/day1.js
+++ src/day1/day1.js
@@ -1,4 +1,10 @@
 import {readFileSync} from "fs";
 
 const data = readFileSync('input.txt', {encoding: "utf-8"});
-console.log(data);
+const lines = data.split("\n");
+
+const digits = lines.map(line => line.match(/\d/g));
+console.log(digits.reduce((sum, digit) => {
+    const num = digit.slice(0, 1) + digit.slice(-1);
+    return sum + parseInt(num);
+}, 0));
