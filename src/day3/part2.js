47f3d698aabe681df6c468468402217d8c4ccd55
Michael Dermis <michaelzd95@gmail.com>
2023-12-08 16:00:22 -0500

    day 3 part 1

---
+++ src/day3/part2.js
@@ -0,0 +1,53 @@
+import {readFileSync} from "fs";
+
+const input = readFileSync("input.txt", {encoding: "utf8"}).split("\r\n");
+let sum = 0;
+const rowLength = input[0].length;
+
+function getSurrounding(i, j) {
+    /** @type{string[]} */
+    let surrounding = [];
+    if (i > 0) {
+        if (j > 0) {
+            surrounding.push(input[i - 1][j - 1]);
+        }
+        surrounding.push(input[i - 1][j]);
+        if (j < rowLength - 1) {
+            surrounding.push(input[i - 1][j + 1]);
+        }
+    }
+    if (j > 0) {
+        surrounding.push(input[i][j - 1]);
+    }
+    if (j < rowLength - 1) {
+        surrounding.push(input[i][j + 1]);
+    }
+    if (i < input.length - 1) {
+        if (j > 0) {
+            surrounding.push(input[i + 1][j - 1]);
+        }
+        surrounding.push(input[i + 1][j]);
+        if (j < rowLength - 1) {
+            surrounding.push(input[i + 1][j + 1]);
+        }
+    }
+    return surrounding;
+}
+
+input.forEach((row, i) => {
+    let part = false;
+    let num = '';
+    [...row].forEach((char, j) => {
+        if (char === '*') {
+            const surroundings = getSurrounding(i, j);
+            if (surroundings.some(char => char.match(/[0-9]/))) {
+                part = true;
+            }
+        }
+    });
+    if (part) {
+        sum += parseInt(num);
+    }
+});
+
+console.log(sum);
