2bc1773233ae8f31800847b5580529634ec8c976
Michael Dermis <michaelzd95@gmail.com>
2023-12-11 21:31:31 -0500

    day 5 part 1 read input

--- 2023/day5/part1.js
+++ 2023/day5/part1.js
@@ -2,4 +2,25 @@ import {readFileSync} from "fs";
 
 const input = readFileSync("input.txt", {encoding: "utf8"});
 
+const seeds = input.match(/seeds: (.*?)\n/)[1].split(" ").map(seed => parseInt(seed));
+
+function readMap(header) {
+    return input.match(new RegExp(`${header}:\n((.|\n)*?)(\n\n|$)`))[1].split("\n").map(map => {
+        const parts = map.split(" ");
+        return {
+            destStart: parseInt(parts[0]),
+            srcStart: parseInt(parts[1]),
+            range: parseInt(parts[2]),
+        };
+    });
+}
+
+const seedsToSoil = readMap('seed-to-soil map');
+const soilToFertilizer = readMap('soil-to-fertilizer map');
+const fertilizerToWater = readMap('fertilizer-to-water map');
+const waterToLight = readMap('water-to-light map');
+const lightToTemperature = readMap('light-to-temperature map');
+const temperatureToHumidity = readMap('temperature-to-humidity map');
+const humidityToLocation = readMap('humidity-to-location map');
+
 
