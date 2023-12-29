01b51a3213790aa39bca5afb5c0ec24da00c8638
Michael Dermis <michaelzd95@gmail.com>
2023-12-28 21:27:49 -0500

    day 7 part 1

--- 2023/day7/part1.js
+++ 2023/day7/part1.js
@@ -10,13 +10,48 @@ const TYPE_VALUES = {
     HIGH_CARD: 1,
 };
 
+const CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+
 /**
  * @param {string[]} hand
  */
 function checkType(hand) {
-    if (hand.every(h => h === hand[0])) {
-        return TYPE_VALUES.FIVE_KIND;
+    const cards = {};
+    hand.forEach(value => {
+        if (cards[value]) {
+            cards[value]++;
+        } else {
+            cards[value] = 1;
+        }
+    });
+
+    switch (Object.keys(cards).length) {
+        case 1:
+            return TYPE_VALUES.FIVE_KIND;
+        case 2:
+            if (Object.values(cards).some(value => value === 4)) {
+                return TYPE_VALUES.FOUR_KIND;
+            } else {
+                return TYPE_VALUES.FULL_HOUSE;
+            }
+        case 3:
+            if (Object.values(cards).some(value => value === 3)) {
+                return TYPE_VALUES.THREE_KIND;
+            } else {
+                return TYPE_VALUES.TWO_PAIR;
+            }
+        case 4:
+            return TYPE_VALUES.ONE_PAIR;
+        default:
+            return TYPE_VALUES.HIGH_CARD;
     }
+}
+
+/**
+ * @param {string} card
+ */
+function checkValue(card) {
+    return CARD_VALUES.findIndex(value => card === value);
 }
 
 const hands = readFileSync("input.txt", {encoding: "utf8"}).split("\n").map(line => {
@@ -28,7 +63,22 @@ const hands = readFileSync("input.txt", {encoding: "utf8"}).split("\n").map(line
 });
 
 hands.sort((a, b) => {
-    return checkType(a.hand) - checkType(b.hand);
+    let typeComp = checkType(a.hand) - checkType(b.hand);
+    if (typeComp !== 0) {
+        return typeComp;
+    }
+
+    for (let i = 0; i < a.hand.length; i++) {
+        const valComp = checkValue(a.hand[i]) - checkValue(b.hand[i]);
+        if (valComp !== 0) {
+            return valComp;
+        }
+    }
+
+    console.error("Could not sort hands", a, b);
+    process.exit(1);
 });
 
-console.log(hands);
+const winnings = hands.reduce((prev, current, currentIndex) => prev + (current.bid * (currentIndex + 1)), 0);
+
+console.log(winnings);
