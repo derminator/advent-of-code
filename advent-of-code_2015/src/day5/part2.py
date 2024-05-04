552e8c9a214924a841672bc8725b085243124267
Michael <michaelzd95@gmail.com>
2024-05-04 18:01:26 -0400

    2015 day 5 part 2 final

--- advent-of-code_2015/src/day5/part2.py
+++ advent-of-code_2015/src/day5/part2.py
@@ -6,18 +6,17 @@ def is_nice(text: str) -> bool:
     prev2 = ""
     pairs = []
     for char in text:
-        if not duplicate_letters_found:
-            if char == prev2:
-                duplicate_letters_found = True
+        if not duplicate_letters_found and char == prev2:
+            duplicate_letters_found = True
 
-            if not pair_found and prev:
-                pair = prev + char
-                if pair in pairs[:-1]:
-                    pair_found = True
-                pairs.append(pair)
+        if not pair_found and prev:
+            pair = prev + char
+            if pair in pairs[:-1]:
+                pair_found = True
+            pairs.append(pair)
 
-            prev2 = prev
-            prev = char
+        prev2 = prev
+        prev = char
 
     if pair_found and duplicate_letters_found:
         return True
