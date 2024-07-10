1f093cc599b32a6e631e288ef274fd8cb5fa7cb4
Michael <michaelzd95@gmail.com>
2024-07-09 20:34:19 -0400

    2022 day 2 move some code

--- advent-of-code_2022/day_2/part_2.go
+++ advent-of-code_2022/day_2/part_2.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"advent-of-code_2022/day_2/lib"
 	"advent-of-code_2022/shared"
 	"fmt"
 )
+
+func getScore(round string) uint {
+	score := uint(0)
+	result, them := lib.ReadRound(round)
+	var you uint8
+	switch result {
+	case 'Y':
+		score += 3
+	case 'Z':
+		score += 6
+	}
+
+	return score
+}
 
 func main() {
 	rounds := shared.ReadFileLines("day_2/input.txt")
 	score := uint(0)
 
 	for _, round := range rounds {
-		fmt.Println(round)
-		// TODO: Score
+		score += getScore(round)
 	}
 
 	fmt.Println(score)
