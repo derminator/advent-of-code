ad0c6fb1afe232de6f4a5d755e64565ef685b6a3
Michael <michaelzd95@gmail.com>
2024-09-02 12:31:00 -0400

    2015 day 6 part 2

--- advent-of-code_2015/src/day6/part2.py
+++ advent-of-code_2015/src/day6/part2.py
@@ -1,15 +1,16 @@
 from day6.shared import parse_file, LIGHTS_GRID_SIZE
 
-lights = [[False for x in range(LIGHTS_GRID_SIZE)] for y in range(LIGHTS_GRID_SIZE)]
+lights = [[0 for x in range(LIGHTS_GRID_SIZE)] for y in range(LIGHTS_GRID_SIZE)]
 
 
 def perform_instruction(op: str, x: int, y: int) -> None:
     if op == "turn on":
-        lights[x][y] = True
+        lights[x][y] += 1
     elif op == "turn off":
-        lights[x][y] = False
+        if lights[x][y] > 0:
+            lights[x][y] -= 1
     elif op == "toggle":
-        lights[x][y] = not lights[x][y]
+        lights[x][y] += 2
     else:
         raise KeyError(f"Unknown operation: {op}")
 
