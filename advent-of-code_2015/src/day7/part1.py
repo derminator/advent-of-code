12b8e5deb5d21d1480f957650f780a091f926768
Michael <michaelzd95@gmail.com>
2024-09-21 20:38:00 -0400

    attempt

--- advent-of-code_2015/src/day7/part1.py
+++ advent-of-code_2015/src/day7/part1.py
@@ -1,18 +1,50 @@
 import re
 from ctypes import c_ushort
+from idlelib.configdialog import is_int
 from typing import Dict, Union
 
-PATTERN = re.compile(r"([a-z]+)? ?(AND|OR|LSHIFT|RSHIFT|NOT) ([a-z\d]+)")
+PATTERN = re.compile(r"([a-z\d]+)? ?(AND|OR|LSHIFT|RSHIFT|NOT) ([a-z\d]+)")
 
 instructions: Dict[str, Union[str, int]] = {}
 
 
 def replace_var(var: Union[str, int, None]) -> Union[int, None]:
-    if var is None or type(var) is int:
+    if var is None:
         return var
+    if is_int(var):
+        return int(var)
     found_instruction = instructions.get(var)
     if found_instruction and type(found_instruction) is int:
         return found_instruction
+
+
+def parse_instruction(instr_in: str) -> Union[str, int]:
+    match = PATTERN.match(instr_in)
+    if match is None:
+        found_instruction = instructions.get(instr_in)
+        if type(found_instruction) is int:
+            return found_instruction
+        else:
+            return instr_in
+    left = replace_var(match.group(1))
+    op = match.group(2)
+    right = replace_var(match.group(3))
+    if type(right) is int:
+        if op == "NOT":
+            instr_in = ~ right
+        elif type(left) is int:
+            if op == "AND":
+                instr_in = left & right
+            elif op == "OR":
+                instr_in = left | right
+            elif op == "LSHIFT":
+                instr_in = left << right
+            elif op == "RSHIFT":
+                instr_in = left >> right
+            else:
+                raise Exception(f"Unknown operator {op}")
+
+    return instr_in
 
 
 with open("input.txt") as file:
@@ -21,26 +53,17 @@ with open("input.txt") as file:
         if instrIn.isnumeric():
             instrIn = int(instrIn)
         else:
-            match = PATTERN.match(instrIn)
-            left = replace_var(match.group(1))
-            op = match.group(2)
-            right = replace_var(match.group(3))
-            if type(right) is int:
-                if op == "NOT":
-                    instrIn = ~ right
-                elif type(left) is int:
-                    if op == "AND":
-                        instrIn = left & right
-                    elif op == "OR":
-                        instrIn = left | right
-                    elif op == "LSHIFT":
-                        instrIn = left << right
-                    elif op == "RSHIFT":
-                        instrIn = left >> right
-                    else:
-                        raise Exception(f"Unknown operator {op}")
+            instrIn = parse_instruction(instrIn)
 
         instructions[instrOut] = instrIn
+
+more_matches = True
+while more_matches:
+    more_matches = False
+    for key, value in instructions.items():
+        if type(value) is str:
+            instructions[key] = parse_instruction(value)
+            more_matches = True
 
 for key, value in instructions.items():
     if type(value) is int:
