a242cb52669288753fb257e11cde1e2c97551f40
Michael <michaelzd95@gmail.com>
2024-12-21 14:39:40 -0500

    2024 day 2 part 2 attempt

--- advent-of-code_2024/src/day2.kt
+++ advent-of-code_2024/src/day2.kt
@@ -1,27 +1,44 @@
 import java.io.File
 import kotlin.math.abs
 
-private fun isReportSafe(report: List<Int>): Boolean {
+private fun isReportSafe(report: List<Int>, useDampener: Boolean): Boolean {
     var prev: Int? = null
     var asc: Boolean? = null
+    var usedDampener = !useDampener
 
     for (level in report) {
         if (prev != null) {
             val diff = abs(level - prev)
-            if (diff < 1 || diff > 3) return false
+            if (diff < 1 || diff > 3) {
+                if (usedDampener) return false
+                else usedDampener = true
+            }
             else if (asc == null) asc = level > prev
-            else if (asc != (level > prev)) return false
+            else if (asc != (level > prev)) {
+                if (usedDampener) return false
+                else usedDampener = true
+            }
         }
         prev = level
     }
     return true
 }
 
+private fun readReports(): List<List<Int>> {
+    return splitFileLines(File(".aoc/2024/2")).map { it.map { s -> s.toInt() } }
+}
+
 private fun part1() {
-    val reports = splitFileLines(File(".aoc/2024/2")).map { it.map { s -> s.toInt() } }
-    println(reports.count { isReportSafe(it) })
+    val reports = readReports()
+    println(reports.count { isReportSafe(it, false) })
+}
+
+private fun part2() {
+    val reports = readReports()
+    println(reports.count { isReportSafe(it, true) })
 }
 
 fun main() {
     part1()
+    part2()
 }
