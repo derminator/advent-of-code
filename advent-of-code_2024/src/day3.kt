29c676a02d7b2e4b9c39d1eb339d7c26a5c046a7
michael <michaelzd95@gmail.com>
2024-12-23 21:26:18 -0500

    2024 day 3 part 2

--- advent-of-code_2024/src/day3.kt
+++ advent-of-code_2024/src/day3.kt
@@ -13,6 +13,29 @@ private fun part1() {
     println(sum)
 }
 
+private fun part2() {
+    val input = File(".aoc/2024/3").readText()
+    val regex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)")
+    val matches = regex.findAll(input)
+    var sum = 0
+    var enabled = true
+    matches.forEach {
+        if (it.value.startsWith("mul")) {
+            if (enabled) {
+                val left = it.groupValues[1].toInt()
+                val right = it.groupValues[2].toInt()
+                sum += left * right
+            }
+        } else if (it.value.startsWith("don")) {
+            enabled = false
+        } else {
+            enabled = true
+        }
+    }
+    println(sum)
+}
+
 fun main() {
     part1()
+    part2()
 }
