1ce88baea33ed8d87c9dc5852c075192591cfa29
michael <michaelzd95@gmail.com>
2024-12-28 14:38:34 -0500

    2024 day 5 attempt

---
+++ advent-of-code_2024/src/day5.kt
@@ -0,0 +1,28 @@
+import java.io.File
+
+val fileSections = File(".aoc/2024/5").readText().split("\n\n")
+
+private fun isUpdateValid(update: List<String>, rules: Map<String, String>): Boolean {
+    update.forEachIndexed({i, value ->
+        run {
+            if (rules.containsKey(value)) {
+                val otherValueIndex = update.indexOf(rules[value])
+                if (otherValueIndex != -1 && i > otherValueIndex) return false
+            }
+        }
+    })
+    return true
+}
+
+private fun part1() {
+    val (rulesText, updatesText) = fileSections
+    val rules = rulesText.lines().associate { it.split("|").let { (key, value) -> key to value } }
+    val updates = updatesText.lines().map { it.split(",") }
+    val validUpdates = updates.filter { isUpdateValid(it, rules) }
+    val middleValues = validUpdates.map { it[it.size / 2] }
+    println(middleValues.sumOf { it.toInt() })
+}
+
+fun main() {
+    part1()
+}
