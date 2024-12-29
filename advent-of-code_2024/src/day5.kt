880cf68cbfa4c37757e0e086f8b1bdac4628cc52
Michael <michaelzd95@gmail.com>
2024-12-29 15:55:10 -0500

    2024 day 5 part 2 start

--- advent-of-code_2024/src/day5.kt
+++ advent-of-code_2024/src/day5.kt
@@ -1,6 +1,8 @@
 import java.io.File
 
 val fileSections = File(".aoc/2024/5").readText().trim().split("\n\n")
+val rules = fileSections[0].lines().map { it.split("|").let { (key, value) -> key to value } }
+val updates = fileSections[1].lines().map { it.split(",") }
 
 private fun isUpdateValid(update: List<String>, rules: List<Pair<String, String>>): Boolean {
     for ((i, value) in update.withIndex()) {
@@ -13,14 +15,23 @@ private fun isUpdateValid(update: List<String>, rules: List<Pair<String, String>
 }
 
 private fun part1() {
-    val (rulesText, updatesText) = fileSections
-    val rules = rulesText.lines().map { it.split("|").let { (key, value) -> key to value } }
-    val updates = updatesText.lines().map { it.split(",") }
     val validUpdates = updates.filter { isUpdateValid(it, rules) }
     val middleValues = validUpdates.map { it[it.size / 2] }
     println(middleValues.sumOf { it.toInt() })
 }
 
+private val updateComparator = Comparator<String> { a, b ->
+    0 // TODO
+}
+
+private fun part2() {
+    val invalidUpdates = updates.filter { !isUpdateValid(it, rules) }
+    val sortedLists = invalidUpdates.map { it.sortedWith(updateComparator) }
+    val middleValues = sortedLists.map { it[it.size / 2] }
+    println(middleValues.sumOf { it.toInt() })
+}
+
 fun main() {
     part1()
+    part2()
 }
