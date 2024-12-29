6c6583a69becf782e3185b3c5b9234dc86a5ca86
Michael <michaelzd95@gmail.com>
2024-12-29 15:42:42 -0500

    2024 day 5 part 1

--- advent-of-code_2024/src/day5.kt
+++ advent-of-code_2024/src/day5.kt
@@ -2,21 +2,19 @@ import java.io.File
 
 val fileSections = File(".aoc/2024/5").readText().trim().split("\n\n")
 
-private fun isUpdateValid(update: List<String>, rules: Map<String, String>): Boolean {
-    update.forEachIndexed({i, value ->
-        run {
-            if (rules.containsKey(value)) {
-                val otherValueIndex = update.indexOf(rules[value])
-                if (otherValueIndex != -1 && i > otherValueIndex) return false
-            }
+private fun isUpdateValid(update: List<String>, rules: List<Pair<String, String>>): Boolean {
+    for ((i, value) in update.withIndex()) {
+        for (rule in rules.filter { it.first == value }) {
+            val otherValueIndex = update.indexOf(rule.second)
+            if (otherValueIndex != -1 && i > otherValueIndex) return false
         }
-    })
+    }
     return true
 }
 
 private fun part1() {
     val (rulesText, updatesText) = fileSections
-    val rules = rulesText.lines().associate { it.split("|").let { (key, value) -> key to value } }
+    val rules = rulesText.lines().map { it.split("|").let { (key, value) -> key to value } }
     val updates = updatesText.lines().map { it.split(",") }
     val validUpdates = updates.filter { isUpdateValid(it, rules) }
     val middleValues = validUpdates.map { it[it.size / 2] }
