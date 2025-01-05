ed5d6d6fb7bd082cd0fc92dc5f9ed27022710189
Michael <michaelzd95@gmail.com>
2025-01-04 19:15:06 -0500

    2024 day 7 part 2

--- advent-of-code_2024/day7.kt
+++ advent-of-code_2024/day7.kt
@@ -21,6 +21,24 @@ private fun part1() {
     println(validCalibrations.sumOf { it.first })
 }
 
+private fun part2() {
+    val validCalibrations = equations.filter { (result, values) ->
+        var possiblePrevValues = listOf(values[0])
+        for (i in 1 until values.size) {
+            val newPrevValues = ArrayList<Long>()
+            possiblePrevValues.forEach {
+                newPrevValues.add(it + values[i])
+                newPrevValues.add(it * values[i])
+                newPrevValues.add((it.toString() + values[i].toString()).toLong())
+            }
+            possiblePrevValues = newPrevValues
+        }
+        possiblePrevValues.contains(result)
+    }
+    println(validCalibrations.sumOf { it.first })
+}
+
 fun main() {
     part1()
+    part2()
 }
