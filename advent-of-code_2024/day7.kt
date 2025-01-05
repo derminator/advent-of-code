4483090de72170ab829c11815099c33314d190a7
Michael <michaelzd95@gmail.com>
2025-01-04 19:12:02 -0500

    2024 day 7 part 1

---
+++ advent-of-code_2024/day7.kt
@@ -0,0 +1,26 @@
+import java.io.File
+
+val equations = File(".aoc/2024/7").readLines().map { line ->
+    val (key, value) = line.split(":")
+    key.trim().toLong() to value.trim().split("\\s+".toRegex()).map { it.toLong() }
+}
+
+private fun part1() {
+    val validCalibrations = equations.filter { (result, values) ->
+        var possiblePrevValues = listOf(values[0])
+        for (i in 1 until values.size) {
+            val newPrevValues = ArrayList<Long>()
+            possiblePrevValues.forEach {
+                newPrevValues.add(it + values[i])
+                newPrevValues.add(it * values[i])
+            }
+            possiblePrevValues = newPrevValues
+        }
+        possiblePrevValues.contains(result)
+    }
+    println(validCalibrations.sumOf { it.first })
+}
+
+fun main() {
+    part1()
+}
