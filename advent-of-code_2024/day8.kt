6be95ba08c75bcd92d44e862b13bf691fa13c441
Michael <michaelzd95@gmail.com>
2025-01-06 13:32:38 -0500

    2024 day 8 part 1

--- advent-of-code_2024/day8.kt
+++ advent-of-code_2024/day8.kt
@@ -15,7 +15,24 @@ private fun part1() {
     val antinodes = map.mapIndexed { i, row ->
         row.mapIndexed { j, _ ->
             antennas.values.any { list ->
-                false // TODO
+                // Check if list contains 2 points that are in line with (i,j) and one is twice as far as the other from (i,j)
+                list.any { (x, y) ->
+                    list.any { (x2, y2) ->
+                        if ((x == x2 && y == y2) || (x == i && y == j) || (x2 == i && y2 == j)) {
+                            false
+                        } else {
+                            val dx = x - i
+                            val dy = y - j
+                            val dx2 = x2 - i
+                            val dy2 = y2 - j
+                            val d1 = dx * dx + dy * dy
+                            val d2 = dx2 * dx2 + dy2 * dy2
+                            val dx3 = x - x2
+                            val dy3 = y - y2
+                            (d1 / 4 == d2 || d2 / 4 == d1) && (dx * dy2 == dx2 * dy && dx * dy3 == dx3 * dy)
+                        }
+                    }
+                }
             }
         }
     }
