213b89dd18dea80909ecdee22b5db03711e68342
Michael <michaelzd95@gmail.com>
2025-03-02 15:48:22 -0500

    2024 day 11 part 2 final

--- advent-of-code_2024/day11.kt
+++ advent-of-code_2024/day11.kt
@@ -1,33 +1,49 @@
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import java.io.File
 
-var stones = File(".aoc/2024/11").readText().trim().split(" ").map { it.toLong() }.asSequence()
+var stones = mutableMapOf<Long, Long>().apply {
+    File(".aoc/2024/11").readText().trim().split(" ").map { it.toLong() }.forEachIndexed { _, stone ->
+        this[stone] = this.getOrDefault(stone, 0L) + 1
+    }
+}
 
-private fun changeStone(stone: Long): Sequence<Long> {
-    if (stone == 0L) return sequenceOf(1)
+private fun changeStone(stone: Long): List<Long> {
+    if (stone == 0L) return listOf(1)
     val stoneStr = stone.toString()
-    return if (stoneStr.length % 2 == 0) sequenceOf(
+    return if (stoneStr.length % 2 == 0) listOf(
         stoneStr.substring(0, stoneStr.length / 2).toLong(),
         stoneStr.substring(stoneStr.length / 2).toLong()
     )
-    else sequenceOf(stone * 2024)
+    else listOf(stone * 2024)
+}
+
+private fun blink() {
+    val newStones = mutableMapOf<Long, Long>().apply {
+        stones.forEach { (stone, count) ->
+            changeStone(stone).forEach { newStone ->
+                this[newStone] = this.getOrDefault(newStone, 0L) + count
+            }
+        }
+    }
+    stones = newStones
+}
+
+private fun countStones() {
+    println(stones.values.sum())
 }
 
 private fun part1() {
     repeat(25) {
-        stones = stones.flatMap { changeStone(it) }
+        blink()
     }
-    println(stones.count())
+    countStones()
 }
 
 private fun part2() = runBlocking {
     repeat(50) {
-        stones = stones.map { stone -> async(Dispatchers.Default) { changeStone(stone) } }
-            .flatMap { newList -> runBlocking { newList.await() } }
+        blink()
     }
-    println(stones.fold(0L) { acc, _ -> acc + 1 })
+    countStones()
 }
 
 fun main() {
