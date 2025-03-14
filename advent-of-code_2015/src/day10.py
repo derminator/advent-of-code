c02b0ab5287f5900d55ef3dd4278db3a35cac71b
Michael <michaelzd95@gmail.com>
2025-03-14 16:36:05 -0400

    2015 day 10 part 1

---
+++ advent-of-code_2015/src/day10.py
@@ -0,0 +1,20 @@
+sequence = "1321131112"
+
+
+def look_and_say():
+    result = ""
+    i = 0
+    while i < len(sequence):
+        count = 1
+        while i + 1 < len(sequence) and sequence[i] == sequence[i + 1]:
+            i += 1
+            count += 1
+        result += str(count) + sequence[i]
+        i += 1
+    return result
+
+
+for _ in range(40):
+    sequence = look_and_say()
+
+print(len(sequence))
