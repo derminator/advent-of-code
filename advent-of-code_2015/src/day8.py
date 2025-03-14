b37652d05025fdb40dbbe0ea5e22db48dde1cbbc
Michael <michaelzd95@gmail.com>
2025-03-14 18:19:43 -0400

    2015 day 8 part 1

--- advent-of-code_2015/src/day8.py
+++ advent-of-code_2015/src/day8.py
@@ -1,15 +1,12 @@
-import codecs
-
 code_chars = 0
 memory_chars = 0
 
-with open("input.txt") as file:
+with open("../../.aoc/2015/8") as file:
     for string in file:
         string = string.strip("\n")
         code_chars += len(string)
-        string_bytes = string[1:-1].encode()
-        decoded_string = codecs.escape_decode(string_bytes)
-        memory_chars += len(decoded_string)
+        parsed_string = eval(string)
+        memory_chars += len(parsed_string)
 
 print(f"Code: {code_chars}")
 print(f"Memory: {memory_chars}")
