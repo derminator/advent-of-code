2ffb99183a873dd5e32d6954db70101330ff3a4b
Michael Dermis <michaelzd95@gmail.com>
2025-04-26 13:46:27 -0400

    2015 day 19 part 2

--- advent-of-code_2015/src/day19.py
+++ advent-of-code_2015/src/day19.py
@@ -2,8 +2,9 @@ with open('../../.aoc/2015/19') as f:
     replacements, molecule = f.read().split("\n\n", 2)
 
 replacements = replacements.splitlines()
-replacements = map(lambda x: x.split(" => "), replacements)
+replacements = list(map(lambda x: x.split(" => "), replacements))
 
+# Part 1: Find all possible molecules after one replacement
 possible_molecules = set()
 for r in replacements:
     rule_in, rule_out = r
@@ -16,4 +17,28 @@ for r in replacements:
             molecule[:index] + rule_out + molecule[index + len(rule_in):]
         )
         start = index + 1
-print(len(possible_molecules))
+print("Part 1:", len(possible_molecules))
+
+# Part 2: Find minimum steps to go from 'e' to the medicine molecule
+# Reverse the replacements (we'll work backwards from the medicine to 'e')
+reverse_replacements = [(rule_out, rule_in) for rule_in, rule_out in replacements]
+# Sort by length of the 'from' part in descending order to replace longer sequences first
+reverse_replacements.sort(key=lambda x: len(x[0]), reverse=True)
+
+# Greedy approach: always replace the longest match first
+current = molecule.strip()
+steps = 0
+
+while current != 'e':
+    for rule_from, rule_to in reverse_replacements:
+        if rule_from in current:
+            # Replace only the first occurrence
+            current = current.replace(rule_from, rule_to, 1)
+            steps += 1
+            break
+    else:
+        # If no replacement was made in this iteration, we're stuck
+        print("No solution found")
+        break
+
+print("Part 2:", steps)
