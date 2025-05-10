3148a30a01b5594c7fbb0f0c52ee5773592a4cff
Michael <michaelzd95@gmail.com>
2025-05-10 14:06:39 -0400

    2015 day 23 part 2

--- advent-of-code_2015/src/day23.py
+++ advent-of-code_2015/src/day23.py
@@ -1,11 +1,3 @@
-from typing import Literal
-
-a = 0
-b = 0
-
-REG = Literal['a', 'b']
-
-
 def hlf(value: int):
     return value // 2
 
@@ -21,42 +13,51 @@ def inc(value: int):
 with open('../../.aoc/2015/23') as f:
     instructions = f.readlines()
 
-instruction_ptr = 0
+
+class Program:
+    def __init__(self, a_val: int):
+        self.instruction_ptr = 0
+        self.registers = {'a': a_val, 'b': 0}
+        self.instruction_ptr = 0
+
+    def _parse_instruction(self, instruction: str):
+        instruction_jump = 1
+        parts = instruction.strip().split(" ")
+        instruction = parts[0]
+        if instruction == "hlf":
+            reg = parts[1]
+            value = self.registers[reg]
+            self.registers[reg] = hlf(value)
+        elif instruction == "tpl":
+            reg = parts[1]
+            value = self.registers[reg]
+            self.registers[reg] = tpl(value)
+        elif instruction == "inc":
+            reg = parts[1]
+            value = self.registers[reg]
+            self.registers[reg] = inc(value)
+        elif instruction == "jmp":
+            instruction_jump = int(parts[1])
+        elif instruction == "jie":
+            reg = parts[1][:-1]
+            if self.registers[reg] % 2 == 0:
+                instruction_jump = int(parts[2])
+        elif instruction == "jio":
+            reg = parts[1][:-1]
+            if self.registers[reg] == 1:
+                instruction_jump = int(parts[2])
+        else:
+            raise Exception(f"Unknown instruction: {instruction}")
+        self.instruction_ptr += instruction_jump
+
+    def run(self):
+        while len(instructions) > self.instruction_ptr >= 0:
+            self._parse_instruction(instructions[self.instruction_ptr])
+        print(self.registers["b"])
 
 
-def parse_instruction(instruction: str):
-    global instruction_ptr
-    instruction_jump = 1
-    parts = instruction.strip().split(" ")
-    instruction = parts[0]
-    if instruction == "hlf":
-        reg = parts[1]
-        value = globals()[reg]
-        globals()[reg] = hlf(value)
-    elif instruction == "tpl":
-        reg = parts[1]
-        value = globals()[reg]
-        globals()[reg] = tpl(value)
-    elif instruction == "inc":
-        reg = parts[1]
-        value = globals()[reg]
-        globals()[reg] = inc(value)
-    elif instruction == "jmp":
-        instruction_jump = int(parts[1])
-    elif instruction == "jie":
-        reg = parts[1][:-1]
-        if globals()[reg] % 2 == 0:
-            instruction_jump = int(parts[2])
-    elif instruction == "jio":
-        reg = parts[1][:-1]
-        if globals()[reg] == 1:
-            instruction_jump = int(parts[2])
-    else:
-        raise Exception(f"Unknown instruction: {instruction}")
-    instruction_ptr += instruction_jump
+part1 = Program(0)
+part1.run()
 
-
-while len(instructions) > instruction_ptr >= 0:
-    parse_instruction(instructions[instruction_ptr])
-
-print(b)
+part2 = Program(1)
+part2.run()
