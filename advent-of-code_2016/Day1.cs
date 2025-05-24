f8cb9a1ff7a3a408cafe40f0759d52e42e73c445
Michael <michaelzd95@gmail.com>
2025-05-24 19:47:10 -0400

    2016 day 1 part 2 attempt

--- advent-of-code_2016/Day1.cs
+++ advent-of-code_2016/Day1.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using JetBrains.Annotations;
 
 namespace advent_of_code_2016
 {
@@ -8,10 +10,12 @@ namespace advent_of_code_2016
         private static readonly string[] Instructions = File.ReadAllText("../../../.aoc/2016/1").Split(
             new[] { ", " }, StringSplitOptions.None);
 
-        private Direction direction = Direction.Up;
-        private int horizontalPosition;
+        private static readonly List<Tuple<int, int>> _visited = new List<Tuple<int, int>>();
 
-        private int verticalPosition;
+        private Direction _direction = Direction.Up;
+        private int _horizontalPosition;
+
+        private int _verticalPosition;
 
         private void FollowInstruction(string instruction)
         {
@@ -19,23 +23,39 @@ namespace advent_of_code_2016
             var distance = int.Parse(instruction.Substring(1));
             // Adjust direction
             if (turn == 'R')
-                direction = (Direction)(((int)direction + 1) % 4);
+                _direction = (Direction)(((int)_direction + 1) % 4);
             else
-                direction = (Direction)(((int)direction - 1 + 4) % 4);
+                _direction = (Direction)(((int)_direction - 1 + 4) % 4);
 
             // Move
-            if (direction == Direction.Up || direction == Direction.Down)
-                verticalPosition += (direction == Direction.Up ? 1 : -1) * distance;
+            if (_direction == Direction.Up || _direction == Direction.Down)
+                _verticalPosition += (_direction == Direction.Up ? 1 : -1) * distance;
             else
-                horizontalPosition += (direction == Direction.Right ? 1 : -1) * distance;
+                _horizontalPosition += (_direction == Direction.Right ? 1 : -1) * distance;
+        }
+
+        private int FindDistance()
+        {
+            return Math.Abs(_horizontalPosition) + Math.Abs(_verticalPosition);
         }
 
         public static void Run()
         {
             var map = new Day1();
-            foreach (var instruction in Instructions) map.FollowInstruction(instruction);
+            var part2 = 0;
+            foreach (var instruction in Instructions)
+            {
+                map.FollowInstruction(instruction);
+                var location = new Tuple<int, int>(map._horizontalPosition, map._verticalPosition);
+                if (part2 == 0 && _visited.Contains(location))
+                    part2 = map.FindDistance();
+                else
+                    _visited.Add(location);
+            }
 
-            Console.WriteLine(Math.Abs(map.horizontalPosition) + Math.Abs(map.verticalPosition));
+            var part1 = map.FindDistance();
+            Console.WriteLine(part1);
+            Console.WriteLine(part2);
         }
 
         private enum Direction
@@ -43,7 +63,7 @@ namespace advent_of_code_2016
             Up,
             Right,
             Down,
-            Left
+            [UsedImplicitly] Left
         }
     }
 }
