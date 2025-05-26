a747ffc42b8a75a98c10b3da2ec4f02fb395ac41
Michael <michaelzd95@gmail.com>
2025-05-26 14:21:03 -0400

    2016 day 2 part 2

--- advent-of-code_2016/Day2.cs
+++ advent-of-code_2016/Day2.cs
@@ -6,11 +6,31 @@ namespace advent_of_code_2016
 {
     internal class Day2
     {
-        private static readonly char[][] Keypad = { "123".ToCharArray(), "456".ToCharArray(), "789".ToCharArray() };
+        private static readonly char[][] Part1Keypad =
+            { "123".ToCharArray(), "456".ToCharArray(), "789".ToCharArray() };
+
+        private static readonly char[][] Part2Keypad =
+        {
+            "  1  ".ToCharArray(), " 234 ".ToCharArray(), "56789".ToCharArray(),
+            " ABC ".ToCharArray(), "  D  ".ToCharArray()
+        };
 
         private static readonly string[] Instructions = File.ReadAllLines("../../../.aoc/2016/2");
 
-        private Tuple<int, int> _currentPosition = new Tuple<int, int>(1, 1);
+        private readonly char[][] _keypad;
+
+        private Tuple<int, int> _currentPosition;
+
+        private Day2(char[][] keypad)
+        {
+            _keypad = keypad;
+            for (var x = 0; x < keypad.Length; x++)
+            for (var y = 0; y < keypad[x].Length; y++)
+                if (keypad[x][y] == '5')
+                    _currentPosition = new Tuple<int, int>(x, y);
+
+            if (_currentPosition == null) throw new Exception("Could not find 5");
+        }
 
         private void Move(char direction)
         {
@@ -33,8 +53,9 @@ namespace advent_of_code_2016
                     throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
             }
 
-            if (newPosition.Item1 >= 0 && newPosition.Item1 < Keypad.Length && newPosition.Item2 >= 0 &&
-                newPosition.Item2 < Keypad[0].Length)
+            if (newPosition.Item1 >= 0 && newPosition.Item1 < _keypad.Length && newPosition.Item2 >= 0 &&
+                newPosition.Item2 < _keypad[newPosition.Item1].Length &&
+                _keypad[newPosition.Item1][newPosition.Item2] != ' ')
                 _currentPosition = newPosition;
         }
 
@@ -42,15 +63,21 @@ namespace advent_of_code_2016
         {
             foreach (var instruction in instrLine) Move(instruction);
 
-            return Keypad[_currentPosition.Item1][_currentPosition.Item2];
+            return _keypad[_currentPosition.Item1][_currentPosition.Item2];
         }
 
         public static void Run()
         {
-            var part1 = new Day2();
-            var answer = Instructions.Aggregate("", (current, instruction) =>
-                current + part1.GetDigit(instruction.ToCharArray()));
-            Console.WriteLine(answer);
+            var part1 = new Day2(Part1Keypad);
+            Console.WriteLine(GetCode(part1));
+            var part2 = new Day2(Part2Keypad);
+            Console.WriteLine(GetCode(part2));
+        }
+
+        private static string GetCode(Day2 parser)
+        {
+            return Instructions.Aggregate("", (current, instruction) =>
+                current + parser.GetDigit(instruction.ToCharArray()));
         }
     }
 }
