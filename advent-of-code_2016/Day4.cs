259307d0138cedd76bc8f09fd8e9fa742c755601
Michael <michaelzd95@gmail.com>
2025-05-31 13:59:32 -0400

    2016 day 4 part 1

---
+++ advent-of-code_2016/Day4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace advent_of_code_2016
+{
+    public class Day4
+    {
+        private static readonly Tuple<string, int, string>[] _input = File.ReadAllLines("../../../.aoc/2016/4")
+            .Select(l =>
+            {
+                var lastDashIndex = l.LastIndexOf('-');
+                var idChecksum = l.Substring(lastDashIndex + 1)
+                    .Split(new[] { '[', ']' }, StringSplitOptions.RemoveEmptyEntries);
+                return new Tuple<string, int, string>(
+                    l.Substring(0, lastDashIndex),
+                    Convert.ToInt32(idChecksum[0]),
+                    idChecksum[1]
+                );
+            }).ToArray();
+
+        private static string CalculateChecksum(string name)
+        {
+            var charCounts = new Dictionary<char, int>();
+            foreach (var character in name.Where(character => character != '-'))
+                if (charCounts.TryGetValue(character, out var count))
+                    charCounts[character] = count + 1;
+                else
+                    charCounts.Add(character, 1);
+
+            // Sort by value (count) in descending order, then by key (character) in ascending order
+            var sortedChars = charCounts.OrderByDescending(pair => pair.Value)
+                .ThenBy(pair => pair.Key)
+                .Select(pair => pair.Key)
+                .Take(5); // Take the first 5 characters for checksum
+
+            return new string(sortedChars.ToArray());
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine(_input.Sum(i => CalculateChecksum(i.Item1) == i.Item3 ? i.Item2 : 0));
+        }
+    }
+}
--- advent-of-code_2016/Program.cs
+++ advent-of-code_2016/Program.cs
@@ -1,12 +1,13 @@
 namespace advent_of_code_2016
 {
-    internal class Program
+    internal static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             Day1.Run();
             Day2.Run();
             Day3.Run();
+            Day4.Run();
         }
     }
 }
