b4951e8d913317a26ca32b522b97f9dc43db2a1d
Michael <michaelzd95@gmail.com>
2025-06-06 15:29:38 -0400

    2016 day 6 part 1

---
+++ advent-of-code_2016/Day6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace advent_of_code_2016
+{
+    public class Day6
+    {
+        private static readonly string[] Input = File.ReadAllLines("../../../.aoc/2016/6");
+
+        public static void Run()
+        {
+            var message = new StringBuilder();
+            var wordLen = Input[0].Length;
+            for (var i = 0; i < wordLen; i++)
+            {
+                var stats = new Dictionary<char, int>();
+                foreach (var attempt in Input)
+                {
+                    var c = attempt[i];
+                    stats[c] = stats.TryGetValue(c, out var stat) ? stat + 1 : 1;
+                }
+
+                var maxChar = stats.OrderByDescending(kvp => kvp.Value).First().Key;
+                message.Append(maxChar);
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
