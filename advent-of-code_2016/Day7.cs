dd99fbc94af3e77db00ce82a6fe0b430028aceb1
Michael <michaelzd95@gmail.com>
2025-06-07 14:00:47 -0400

    2016 day 7 part 2

--- advent-of-code_2016/Day7.cs
+++ advent-of-code_2016/Day7.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace advent_of_code_2016
 {
-    public class Day7
+    public static class Day7
     {
         private static readonly string[] Addresses = File.ReadAllLines("../../../.aoc/2016/7");
 
@@ -44,9 +45,47 @@ namespace advent_of_code_2016
             return hasAbba;
         }
 
+        private static bool SupportsSsl(string address)
+        {
+            var inHypernet = false;
+            var hypernetAbas = new List<string>();
+            var supernetAbas = new List<string>();
+            var history = new char?[] { null, null };
+            foreach (var digit in address)
+            {
+                if (digit == '[' || digit == ']') history = new char?[] { null, null };
+                switch (digit)
+                {
+                    case '[':
+                        inHypernet = true;
+                        break;
+                    case ']':
+                        inHypernet = false;
+                        break;
+                    default:
+                        if (history.All(c => c != null) && history[0] == digit && history[1] != digit)
+                        {
+                            var list = inHypernet ? hypernetAbas : supernetAbas;
+                            var compareList = inHypernet ? supernetAbas : hypernetAbas;
+                            var aba = $"{history[0]}{history[1]}{digit}";
+                            var abaReverse = $"{history[1]}{digit}{history[1]}";
+                            if (compareList.Any(i => i == abaReverse)) return true;
+                            list.Add(aba);
+                        }
+
+                        history[0] = history[1];
+                        history[1] = digit;
+                        break;
+                }
+            }
+
+            return false;
+        }
+
         public static void Run()
         {
             Console.WriteLine(Addresses.Count(SupportsTls));
+            Console.WriteLine(Addresses.Count(SupportsSsl));
         }
     }
 }
