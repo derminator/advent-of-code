fa538a74398c6cd3519a7b0f1e469caad7b4dd63
Michael <michaelzd95@gmail.com>
2025-08-29 16:09:09 -0400

    "Implemented optimized solution for Day 15: Used TimeMap to calculate disc positions at time"

--- advent-of-code_2016/Day15.cs
+++ advent-of-code_2016/Day15.cs
@@ -2,18 +2,46 @@
 
 public static class Day15
 {
+    private static readonly List<List<Disc>> TimeMap =
+    [
+        [
+            new Disc(17, 15),
+            new Disc(3, 2),
+            new Disc(19, 4),
+            new Disc(13, 2),
+            new Disc(7, 2),
+            new Disc(5, 0)
+        ]
+    ];
+
     public static void Run()
     {
-        var discs = new List<Disc>
+        var startTime = 0;
+        while (!CheckForSolution(startTime))
         {
-            new(1, 17, 15),
-            new(2, 3, 2),
-            new(3, 19, 4),
-            new(4, 13, 2),
-            new(5, 7, 2),
-            new(6, 5, 0)
-        };
+            startTime++;
+        }
+
+        Console.WriteLine(startTime);
     }
 
-    private record Disc(int Id, int Positions, int StartPosition);
+    private static int GetPositionAtTime(int time, int discId)
+    {
+        while (TimeMap.Count <= time)
+            TimeMap.Add(TimeMap.Last().Select(disc =>
+                disc with { StartPosition = (disc.StartPosition + 1) % disc.Positions }).ToList());
+
+        return TimeMap[time][discId - 1].StartPosition;
+    }
+
+    private static bool CheckForSolution(int startTime)
+    {
+        for (var i = 1; i <= 6; i++)
+            if (GetPositionAtTime(startTime + i, i) != 0)
+                return false;
+
+        return true;
+    }
+
+    private record Disc(int Positions, int StartPosition);
 }
