f194f127fd9ad65e64dc0710f323334889466758
Michael <michaelzd95@gmail.com>
2025-08-29 15:32:01 -0400

    2016 day 15 setup with disc definitions

--- advent-of-code_2016/Day15.cs
+++ advent-of-code_2016/Day15.cs
@@ -4,5 +4,16 @@ public static class Day15
 {
     public static void Run()
     {
+        var discs = new List<Disc>
+        {
+            new(1, 17, 15),
+            new(2, 3, 2),
+            new(3, 19, 4),
+            new(4, 13, 2),
+            new(5, 7, 2),
+            new(6, 5, 0)
+        };
     }
+
+    private record Disc(int Id, int Positions, int StartPosition);
 }
