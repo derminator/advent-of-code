8169f238b1a0be9ebf0d7027a5f2b4e0c9e403a9
Michael <michaelzd95@gmail.com>
2025-09-13 13:06:05 -0400

    2016 day 17 setup with input and direction enum

---
+++ advent-of-code_2016/Day17.cs
@@ -0,0 +1,26 @@
+﻿namespace advent_of_code_2016;
+
+public static class Day17
+{
+    private const string Input = "qljzarfv";
+
+    private static char PathChar(this Direction direction)
+    {
+        return direction switch
+        {
+            Direction.Up => 'U',
+            Direction.Down => 'D',
+            Direction.Left => 'L',
+            Direction.Right => 'R',
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
+    }
+
+    private enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
