d8afe8294a39b3bccb760c81e60c3cf061ea4a7f
Michael <michaelzd95@gmail.com>
2025-09-20 15:23:01 -0400

    "Day 17: Added State class with neighbor validation and pathfinding logic"

--- advent-of-code_2016/Day17.cs
+++ advent-of-code_2016/Day17.cs
@@ -19,6 +19,26 @@ public static class Day17
         };
     }
 
+    public static void Run()
+    {
+        var x = 0;
+        var y = 0;
+        var moves = Array.Empty<char>();
+
+        var queue = new Queue<State>();
+        queue.Enqueue(new State(moves, x, y));
+
+        while (queue.Count > 0)
+        {
+            var state = queue.Dequeue();
+            if (state is { X: 3, Y: 3 })
+            {
+                Console.WriteLine(state.PreviousMoves);
+                return;
+            }
+        }
+    }
+
     private enum Direction
     {
         Up,
@@ -29,7 +49,44 @@ public static class Day17
 
     private class Hash(char[] previousMoves)
     {
-        private string _hash = MD5.HashData(Encoding.UTF8.GetBytes(Input + previousMoves)).ToString()
-                               ?? throw new InvalidOperationException();
+        private readonly string _hash = MD5.HashData(Encoding.UTF8.GetBytes(Input + previousMoves)).ToString()
+                                        ?? throw new InvalidOperationException();
+
+        public bool IsDoorOpen(Direction direction)
+        {
+            var checkChar = _hash[(int)direction];
+            return checkChar is > 'a' and < 'z';
+        }
+    }
+
+    private record State(char[] PreviousMoves, int X, int Y)
+    {
+        public List<State> GetValidNeighbors()
+        {
+            var hash = new Hash(PreviousMoves);
+            return Enum.GetValues<Direction>().Where(dir => hash.IsDoorOpen(dir)).Select(dir =>
+            {
+                var pathChar = dir.PathChar();
+                var newX = X;
+                var newY = Y;
+                switch (dir)
+                {
+                    case Direction.Up:
+                        newY += 1;
+                        break;
+                    case Direction.Down:
+                        newY -= 1;
+                        break;
+                    case Direction.Left:
+                        newX -= 1;
+                        break;
+                    case Direction.Right:
+                        newX += 1;
+                        break;
+                }
+
+                return new State(PreviousMoves.Append(pathChar).ToArray(), newX, newY);
+            }).Where(state => state.X < 0 || state.X > 3 || state.Y < 0 || state.Y > 3).ToList();
+        }
     }
 }
