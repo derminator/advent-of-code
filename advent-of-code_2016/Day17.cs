60f07e4fc0a612655619d4ffc89d7a630b5fa45a
Michael <michaelzd95@gmail.com>
2025-09-26 17:05:32 -0400

    2016 day 17 part 1

--- advent-of-code_2016/Day17.cs
+++ advent-of-code_2016/Day17.cs
@@ -21,22 +21,24 @@ public static class Day17
 
     public static void Run()
     {
-        var x = 0;
-        var y = 0;
         var moves = Array.Empty<char>();
 
         var queue = new Queue<State>();
-        queue.Enqueue(new State(moves, x, y));
+        queue.Enqueue(new State(moves, 0, 0));
 
         while (queue.Count > 0)
         {
             var state = queue.Dequeue();
             if (state is { X: 3, Y: 3 })
             {
-                Console.WriteLine(state.PreviousMoves);
+                Console.WriteLine(new string(state.PreviousMoves));
                 return;
             }
+
+            foreach (var move in state.GetValidNeighbors()) queue.Enqueue(move);
         }
+
+        Console.WriteLine("No valid solution");
     }
 
     private enum Direction
@@ -49,13 +51,15 @@ public static class Day17
 
     private class Hash(char[] previousMoves)
     {
-        private readonly string _hash = MD5.HashData(Encoding.UTF8.GetBytes(Input + previousMoves)).ToString()
+        private readonly string _hash = string.Concat(MD5.HashData(Encoding.UTF8
+                                                .GetBytes(Input + new string(previousMoves)))
+                                            .Select(b => b.ToString("x2")))
                                         ?? throw new InvalidOperationException();
 
         public bool IsDoorOpen(Direction direction)
         {
             var checkChar = _hash[(int)direction];
-            return checkChar is > 'a' and < 'z';
+            return checkChar is >= 'b' and <= 'f';
         }
     }
 
@@ -72,10 +76,10 @@ public static class Day17
                 switch (dir)
                 {
                     case Direction.Up:
-                        newY += 1;
+                        newY -= 1;
                         break;
                     case Direction.Down:
-                        newY -= 1;
+                        newY += 1;
                         break;
                     case Direction.Left:
                         newX -= 1;
@@ -86,7 +90,7 @@ public static class Day17
                 }
 
                 return new State(PreviousMoves.Append(pathChar).ToArray(), newX, newY);
-            }).Where(state => state.X < 0 || state.X > 3 || state.Y < 0 || state.Y > 3).ToList();
+            }).Where(state => state.X is >= 0 and <= 3 && state.Y is >= 0 and <= 3).ToList();
         }
     }
 }
