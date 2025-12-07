1927be21f6948f78bc107cff1515ebe7f117909d
Michael <michaelzd95@gmail.com>
2025-12-07 12:03:40 -0500

    2025 day 1 part 2 attempt

--- advent-of-code_2025/src/bin/day01.rs
+++ advent-of-code_2025/src/bin/day01.rs
@@ -5,25 +5,37 @@ mod puzzle_structs {
     pub struct Dial {
         pos: i32,
         times_stopped_at_0: i32,
+        times_passed_0: i32,
     }
 
     impl Dial {
         pub fn turn(&mut self, amount: i32) {
-            let mut new_pos = (self.pos + amount) % 100;
-            if new_pos < 0 {new_pos = 100 + new_pos};
-            self.pos = new_pos;
+            self.pos += amount;
 
-            if new_pos == 0 {
+            while self.pos > 99 {
+                self.times_passed_0 += 1;
+                self.pos -= 100;
+            }
+            while self.pos < 0 {
+                self.times_passed_0 += 1;
+                self.pos += 100;
+            }
+
+            if self.pos == 0 {
                 self.times_stopped_at_0 += 1;
             }
         }
 
         pub fn new() -> Self {
-            Self { pos: 50, times_stopped_at_0: 0 }
+            Self { pos: 50, times_stopped_at_0: 0, times_passed_0: 0 }
         }
 
         pub fn times_stopped_at_0(&self) -> i32 {
             self.times_stopped_at_0
+        }
+
+        pub fn times_passed_0(&self) -> i32 {
+            self.times_passed_0
         }
     }
 }
@@ -42,5 +54,6 @@ fn main() {
         let amount: i32 = parser.as_str().parse().unwrap();
         dial.turn(amount * direction);
     }
-    println!("{}", dial.times_stopped_at_0());
+    println!("Part 1: {}", dial.times_stopped_at_0());
+    println!("Part 2: {}", dial.times_passed_0());
 }
