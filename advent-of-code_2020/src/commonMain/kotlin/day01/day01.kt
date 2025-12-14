2d83c0c7fca12fb05e0a2e6ed5e0f6fa009819ec
Michael <michaelzd95@gmail.com>
2025-12-14 12:14:05 -0500

    2020 day 1 part 1 more

--- advent-of-code_2020/src/commonMain/kotlin/day01/day01.kt
+++ advent-of-code_2020/src/commonMain/kotlin/day01/day01.kt
@@ -4,5 +4,14 @@ import getInputLines
 
 fun main() {
     val entries = getInputLines(1).map { it.toInt() }
+    println("Part 1: ${findPair(entries)}")
+}
 
+private fun findPair(entries: List<Int>): Pair<Int, Int> {
+    entries.forEach { a ->
+        (entries - a).forEach { b ->
+            if (a + b == 2020) return a to b
+        }
+    }
+    error("No pair found")
 }
