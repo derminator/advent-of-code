105389c327c75e3f3a94c5489f7165b604f4e26c
Michael <michaelzd95@gmail.com>
2025-12-14 18:33:23 -0500

    2020 day 2 part 2

--- advent-of-code_2020/src/commonMain/kotlin/day02/day02.kt
+++ advent-of-code_2020/src/commonMain/kotlin/day02/day02.kt
@@ -2,18 +2,19 @@ package day02
 
 import getInputLines
 
-class PasswordPolicy(input: String) {
-    private val min = input.substringBefore('-').toInt()
-    private val max = input.substringBefore(" ").substringAfter('-').toInt()
+private class PasswordPolicy(input: String) {
+    private val lower = input.substringBefore('-').toInt()
+    private val upper = input.substringBefore(" ").substringAfter('-').toInt()
     private val letter = input.substringAfter(" ").substringBefore(":").single()
     private val password = input.substringAfter(": ")
 
-    fun isValid(): Boolean {
-        return password.count { it == letter } in min..max
-    }
+    val isValidPart1: Boolean = password.count { it == letter } in lower..upper
+
+    val isValidPart2: Boolean = (password[lower - 1] == letter) xor (password[upper - 1] == letter)
 }
 
 fun main() {
     val passwords = getInputLines(2).map(::PasswordPolicy)
-    println("Part 1: ${passwords.count { it.isValid() }}")
+    println("Part 1: ${passwords.count { it.isValidPart1 }}")
+    println("Part 2: ${passwords.count { it.isValidPart2 }}")
 }
