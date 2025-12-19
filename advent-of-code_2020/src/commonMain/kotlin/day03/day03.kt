74f8e879ae755a190658997bb3a1320f3ae7f750
Michael <michaelzd95@gmail.com>
2025-12-19 11:00:05 -0500

    2020 day 3 part 2

--- advent-of-code_2020/src/commonMain/kotlin/day03/day03.kt
+++ advent-of-code_2020/src/commonMain/kotlin/day03/day03.kt
@@ -2,22 +2,33 @@ package day03
 
 import getInputLines
 
-private object Map {
-    private val pattern = getInputLines(3).map { it.map { c -> c == '#' } }
+private val pattern = getInputLines(3).map { it.map { c -> c == '#' } }
+
+private class MapTraverser(private val slope: Pair<Int, Int> = Pair(3, 1)) {
     private var currentRow = 0
     private var currentCol = 0
-    var treesHit = 0
+    var treesHit = 0L
+        private set
 
-    fun moveForward() {
-        currentCol = (currentCol + 3) % pattern[currentRow].size
-        currentRow++
+    private fun moveForward() {
+        currentCol = (currentCol + slope.first) % pattern[currentRow].size
+        currentRow += slope.second
         if (currentRow < pattern.size && pattern[currentRow][currentCol]) treesHit++
     }
 
-    val atEnd get() = currentRow >= pattern.size
+    private val atEnd get() = currentRow >= pattern.size
+
+    fun moveToEnd() {
+        while (!atEnd) moveForward()
+    }
 }
 
 fun main() {
-    while (!Map.atEnd) Map.moveForward()
-    println("Part 1: ${Map.treesHit}")
+    val part1Map = MapTraverser()
+    part1Map.moveToEnd()
+    println("Part 1: ${part1Map.treesHit}")
+
+    val part2Maps = setOf(Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1), Pair(1, 2)).map { MapTraverser(it) }
+    part2Maps.forEach { it.moveToEnd() }
+    println("Part 2: ${part2Maps.map { it.treesHit }.reduce { acc, i -> acc * i }}")
 }
