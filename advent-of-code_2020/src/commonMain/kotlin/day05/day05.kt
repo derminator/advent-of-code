d82c06595651f78b3af9e894dae80d4cd8ffc995
Michael <michaelzd95@gmail.com>
2025-12-20 13:29:03 -0500

    2020 day 5 part 1

---
+++ advent-of-code_2020/src/commonMain/kotlin/day05/day05.kt
@@ -0,0 +1,22 @@
+package day05
+
+import getInputLines
+
+private fun String.findValue(max: Int, low: Char, high: Char): Int = fold(0..max) { acc, ch ->
+    when(ch) {
+        low -> acc.first..<(acc.last - (acc.last - acc.first) / 2)
+        high -> (acc.first + (acc.last - acc.first) / 2 + 1)..acc.last
+        else -> throw IllegalArgumentException("Invalid mapping character $ch")
+    }
+}.single()
+
+private class BoardingPass(passText: String) {
+    val row = passText.substring(0, 7).findValue(127, 'F', 'B')
+    val column = passText.substring(7).findValue(7, 'L', 'R')
+    val seatId = row * 8 + column
+}
+
+fun main() {
+    val passes = getInputLines(5).map { BoardingPass(it) }
+    println("Part 1: ${passes.maxOf { it.seatId }}")
+}
