b867cf6898be9c8d57b9e74a8fe48a7d1cb75205
Michael <michaelzd95@gmail.com>
2025-12-23 10:48:48 -0500

    2020 day 8 part 2

--- advent-of-code_2020/src/commonMain/kotlin/day08/day08.kt
+++ advent-of-code_2020/src/commonMain/kotlin/day08/day08.kt
@@ -2,34 +2,65 @@ package day08
 
 import getInputLines
 
-private var accumulator: Int = 0
-private var instructionPointer: Int = 0
-private val executedInstructions = mutableSetOf<Int>()
+private class Program(private val instructions: List<Instruction>) {
+    var accumulator: Int = 0
+    private var instructionPointer: Int = 0
+    private val executedInstructions = mutableSetOf<Int>()
 
-private class Instruction(assemble: String) {
-    private val operation = assemble.substringBefore(" ")
-    private val argument = assemble.substringAfter(" ").toInt()
-
-    fun execute() {
-        when (operation) {
-            "acc" -> accumulator += argument
+    fun execute(instruction: Instruction) {
+        when (instruction.operation) {
+            "acc" -> accumulator += instruction.argument
             "jmp" -> {
-                instructionPointer += argument
+                instructionPointer += instruction.argument
                 return
             }
 
             "nop" -> {}
-            else -> throw IllegalArgumentException("Invalid operation: $operation")
+            else -> throw IllegalArgumentException("Invalid operation: ${instruction.operation}")
         }
         instructionPointer++
     }
+
+    fun hasLoop(): Boolean {
+        while (instructionPointer !in executedInstructions && instructionPointer < instructions.size) {
+            executedInstructions.add(instructionPointer)
+            execute(instructions[instructionPointer])
+        }
+        return instructionPointer < instructions.size
+    }
+}
+
+private class Instruction(assemble: String) {
+    val operation = assemble.substringBefore(" ")
+    val argument = assemble.substringAfter(" ").toInt()
 }
 
 fun main() {
     val instructions = getInputLines(8).map(::Instruction)
-    while (instructionPointer !in executedInstructions) {
-        executedInstructions.add(instructionPointer)
-        instructions[instructionPointer].execute()
+    Program(instructions).let { program ->
+        program.hasLoop()
+        println("Part 1: ${program.accumulator}")
     }
-    println("Part 1: $accumulator")
+
+    instructions.forEachIndexed { index, instruction ->
+        if (instruction.operation == "nop") {
+            val newInstructions = instructions.toMutableList()
+            newInstructions[index] = Instruction("jmp ${instruction.argument}")
+            Program(newInstructions).let { program ->
+                if (!program.hasLoop()) {
+                    println("Part 2: ${program.accumulator}")
+                    return@forEachIndexed
+                }
+            }
+        } else if (instruction.operation == "jmp") {
+            val newInstructions = instructions.toMutableList()
+            newInstructions[index] = Instruction("nop ${instruction.argument}")
+            Program(newInstructions).let { program ->
+                if (!program.hasLoop()) {
+                    println("Part 2: ${program.accumulator}")
+                    return@forEachIndexed
+                }
+            }
+        }
+    }
 }
