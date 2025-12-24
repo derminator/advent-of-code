8893f504cae1a473b564b2898ec9a3d786faed79
Michael <michaelzd95@gmail.com>
2025-12-24 11:31:31 -0500

    2020 day 9 part 2

--- advent-of-code_2020/src/commonMain/kotlin/day09/day09.kt
+++ advent-of-code_2020/src/commonMain/kotlin/day09/day09.kt
@@ -19,4 +19,15 @@ private fun isValid(index: Int): Boolean {
 fun main() {
     val firstBad = input.indices.first { !isValid(it) }
     println("Part 1: ${input[firstBad]}")
+
+    for (potentialStart in input.indices) {
+        val set = mutableListOf<Long>()
+        for (i in potentialStart until input.lastIndex + 1) {
+            set.add(input[i])
+            if (set.size >= 2 && set.sum() == input[firstBad]) {
+                println("Part 2: ${set.min() + set.max()}")
+                return
+            }
+        }
+    }
 }
