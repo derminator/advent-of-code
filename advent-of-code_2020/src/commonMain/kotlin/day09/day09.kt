b9fd3594825a16f42dd515411a7c64f6d1d223d8
Michael <michaelzd95@gmail.com>
2025-12-24 11:23:47 -0500

    2020 day 9 part 1

---
+++ advent-of-code_2020/src/commonMain/kotlin/day09/day09.kt
@@ -0,0 +1,22 @@
+package day09
+
+import getInputLines
+
+private const val preambleSize = 25
+private val input = getInputLines(9).map { it.toLong() }
+
+private fun isValid(index: Int): Boolean {
+    if (index < preambleSize) return true
+
+    for (i in index - preambleSize until index) {
+        for (j in i + 1 until index) {
+            if (i != j && input[i] + input[j] == input[index]) return true
+        }
+    }
+    return false
+}
+
+fun main() {
+    val firstBad = input.indices.first { !isValid(it) }
+    println("Part 1: ${input[firstBad]}")
+}
