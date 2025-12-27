087a1141caaf5057ada411230e1693cd2fed977f
Michael <michaelzd95@gmail.com>
2025-12-27 11:39:35 -0500

    2025 day 4 part 2

--- advent-of-code_2025/src/bin/day04.rs
+++ advent-of-code_2025/src/bin/day04.rs
@@ -34,13 +34,29 @@ fn main() {
         .iter()
         .map(|line| line.chars().map(|c| c == '@').collect::<Vec<_>>())
         .collect();
-    let mut accessible_papers = 0;
+    let mut removable_papers = get_removable_papers(&grid);
+    println!("Part 1: {}", removable_papers.len());
+
+    let mut removed_papers = 0;
+    let mut current_grid = grid.clone();
+    while removable_papers.len() > 0 {
+        removed_papers += removable_papers.len();
+        for (row, col) in removable_papers.drain(..) {
+            current_grid[row][col] = false;
+        }
+        removable_papers = get_removable_papers(&current_grid);
+    }
+    println!("Part 2: {}", removed_papers);
+}
+
+fn get_removable_papers(grid: &Vec<Vec<bool>>) -> Vec<(usize, usize)> {
+    let mut removable_papers = Vec::new();
     for row in 0..grid.len() {
         for col in 0..grid[row].len() {
             if can_access_roll(&grid, row, col) {
-                accessible_papers += 1;
+                removable_papers.push((row, col));
             }
         }
     }
-    println!("Part 1: {}", accessible_papers);
+    removable_papers
 }
