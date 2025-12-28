7ae06303c55482108d059cb2d0fdcbce31c3f7a0
Michael <michaelzd95@gmail.com>
2025-12-28 14:22:54 -0500

    2025 day 5 part 2 optimized

--- advent-of-code_2025/src/bin/day05.rs
+++ advent-of-code_2025/src/bin/day05.rs
@@ -3,7 +3,7 @@ use advent_of_code::input::read_input_lines;
 fn main() {
     let raw_input = read_input_lines(5);
     let input_sections = raw_input.split(|str| str.is_empty()).collect::<Vec<_>>();
-    let ranges = input_sections[0]
+    let mut ranges = input_sections[0]
         .iter()
         .map(|range| {
             let mut split = range.split('-');
@@ -23,13 +23,18 @@ fn main() {
         .count();
     println!("Part 1: {}", fresh_fruits);
 
-    let mut valid_ids = Vec::new();
-    for range in ranges {
-        for id in range {
-            if !valid_ids.contains(&id) {
-                valid_ids.push(id);
-            }
+    ranges.sort_by_key(|range| *range.start());
+    let mut num_valid_ids = 0u64;
+    let mut range_iter = ranges.iter();
+    let mut current_range = range_iter.next().unwrap().clone();
+    for range in range_iter {
+        if range.start() <= current_range.end() {
+            current_range = *current_range.start()..=*range.end().max(current_range.end());
+        } else {
+            num_valid_ids += current_range.end() - current_range.start() + 1;
+            current_range = range.clone();
         }
     }
-    println!("Part 2: {}", valid_ids.len());
+    num_valid_ids += current_range.end() - current_range.start() + 1;
+    println!("Part 2: {}", num_valid_ids);
 }
