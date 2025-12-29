3e8314e682709c39294e9a6efd50222c28270780
Michael <michaelzd95@gmail.com>
2025-12-29 13:23:53 -0500

    2021 day 1 part 2

--- advent-of-code_2021/src/Day01.java
+++ advent-of-code_2021/src/Day01.java
@@ -5,13 +5,23 @@ public class Day01 {
     public static void main(String[] args) throws IOException {
         int[] depths = Files.readAllLines(Lib.getInputFile(1)).stream().mapToInt(Integer::parseInt).toArray();
         Integer prev = null;
-        int increases = 0;
+        int part1Increases = 0;
         for (int depth : depths) {
             if (prev != null && depth > prev) {
-                increases++;
+                part1Increases++;
             }
             prev = depth;
         }
-        System.out.println("Part 1: " + increases);
+        System.out.println("Part 1: " + part1Increases);
+
+        int part2Increases = 0;
+        for (int i = 3; i < depths.length; i++) {
+            int prevWindow = depths[i - 3] + depths[i - 2] + depths[i - 1];
+            int currWindow = depths[i - 2] + depths[i - 1] + depths[i];
+            if (currWindow > prevWindow) {
+                part2Increases++;
+            }
+        }
+        System.out.println("Part 2: " + part2Increases);
     }
 }
